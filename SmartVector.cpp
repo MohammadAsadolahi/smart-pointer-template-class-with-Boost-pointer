6a9aa55078684d8a6b9182b89e363ab27b9c80da
mohammadAsadolahi <79268727+mohammadAsadolahi@users.noreply.github.com>
2021-06-21 02:44:10 -0700

    Add files via upload

---
+++ SmartVector.cpp
@@ -0,0 +1,56 @@
+/*
+ * SmartVector.cpp
+ *
+ *      Author: mohammad asadolahi
+ *        https://github.com/mohammadAsadolahi
+ */
+
+#include "SmartVector.h"
+template<class Type>
+inline auto SmartVector<Type >::add (Type data) throw (unique_ptr<Exception >)->void
+	{
+		if (count == 0)
+			{
+				root = shared_ptr<Node >
+					{ new Node(data) };
+			}
+		else
+			{
+				auto temp = root;
+				while (temp->nextNode)
+					temp = temp->nextNode;
+				temp->nextNode = shared_ptr<Node >
+					{ new Node(data) };
+			}
+		count++;
+	}
+template<class Type>
+auto SmartVector<Type >::remove (int index) throw (unique_ptr<Exception >)->void
+	{
+		if (count < 1 || index >= count)
+			throw unique_ptr<Exception >
+				{ new Exception("can't remove object, index out of vector") };
+		auto tempRoot = root;
+		for (register int counter = 0 ;counter < index - 1 ;counter++)
+			tempRoot = tempRoot->nextNode;
+		tempRoot->nextNode = tempRoot->nextNode->nextNode;
+		count--;
+	}
+
+template<class Type>
+auto SmartVector<Type >::getCount ( ) throw (unique_ptr<Exception >)->int
+	{
+		return count;
+	}
+
+template<class Type>
+auto SmartVector<Type >::operator [] (int index) throw (unique_ptr<Exception >)->Type&
+	{
+		if (index < 0 || index >= count)
+			throw unique_ptr<Exception >
+				{ new Exception("can't access object, index out of vector") };
+		auto tempRoot = root;
+		for (register int counter = 0 ;counter < index ;counter++)
+			tempRoot = tempRoot->nextNode;
+		return (*tempRoot->data);
+	}
---
+++ SmartVector.h
@@ -0,0 +1,54 @@
+/*
+ * SmartVector.cpp
+ *
+ *      Author: mohammad asadolahi
+ *        https://github.com/mohammadAsadolahi
+ */
+#include <memory>
+#include <iostream>
+#include <ctime>
+using namespace std;
+#ifndef SMARTVECTOR_H_
+#define SMARTVECTOR_H_
+class Exception
+	{
+		string description;
+	public:
+		Exception (string description = "unknown reason") :
+				description(description)
+			{
+			}
+		auto getDescription ( )->string
+			{
+				return description;
+			}
+		auto operator << (ostream& outputStream) const ->ostream&
+			{
+				outputStream << "an error occured! description:\n"
+						<< description << endl;
+				return outputStream;
+			}
+	};
+
+template<class Type> class SmartVector
+	{
+		class Node
+			{
+			public:
+				shared_ptr<Type >data;
+				shared_ptr<Node >nextNode;
+				Node (Type data)
+					{
+						this->data = shared_ptr<Type >(new Type(data));
+						nextNode = nullptr;
+					}
+			};
+		shared_ptr<Node >root;
+		int count = 0;
+	public:
+		auto add (Type data) throw (unique_ptr<Exception >)->void;
+		auto remove (int index) throw (unique_ptr<Exception >)->void;
+		auto getCount ( ) throw (unique_ptr<Exception >)->int;
+		auto operator[] (int index) throw (unique_ptr<Exception >)->Type&;
+	};
+#endif /* SMARTVECTOR_H_ */
